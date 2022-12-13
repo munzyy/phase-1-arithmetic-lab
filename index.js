19eecdabe894c552b0cd67675f7f045665fa017b
Ryan Munz <ryan.munzy@gmail.com>
2022-12-12 19:42:53 -0500

    commit tree 23e5b6180d234bfebacc4027e62eb90ad95ab9f4

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+   
+let num3 = 11;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(18, 19, 20);
 
 // Write your code here
